6c10ad04bd0a5e355796ec5e09cd1b61be0483e6
ChanelT <crene7722@gmail.com>
2021-11-18 21:21:53 -0600

    sin to x and y pos

--- 3_oscillation/harmonic_motion3.5/sin_to_y_pos/sketch.js
+++ 3_oscillation/harmonic_motion3.5/sin_to_y_pos/sketch.js
@@ -1,22 +1,20 @@
-
-let angle = 0
+let angle = 0;
 
 function setup() {
   createCanvas(400, 400);
-  
 }
 
 function draw() {
-  background(0);
-  translate(200,200);
-  fill(252,238,33)
-  let y = map(sin(angle), -1,1,-2,200);
-  let x = map(sin(angle), -1,1,-2,200);
+  background(0, 10);
+  translate(200, 200);
+
+  let y = map(sin(angle), -1, 1, -180, 180);
+  let x = map(sin(angle), -1, 1, -18, 180);
+  fill(255, y, x);
   stroke(255);
-  line(0,0,x,y)
-  circle(x,y,32)
+  line(0, 0, x, y);
+  circle(x, y, 32);
 
   // controls the speed
-  angle += 0.01
+  angle += 0.01;
 }
-
