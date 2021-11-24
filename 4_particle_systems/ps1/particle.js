f75a5a1f346da0391ca7568993d419cb8d8dc25c
ChanelT <crene7722@gmail.com>
2021-11-24 08:49:21 -0600

    4.1 particle system

--- 4_particle_systems/ps1/particle.js
+++ 4_particle_systems/ps1/particle.js
@@ -1,18 +1,58 @@
-export default class Particle{
-    constructor(x,y){
-        thisl.pos = createVector(x,y);
-        this.vel = createVector(0,0);
-        this.acc = createVector(0,0);
-        this.r = 16;
-    }
+class Particle {
+  constructor(x, y) {
+    this.pos = createVector(x, y, color);
+    this.vel = p5.Vector.random2D();
+    // create random burst of particles
+    this.vel.mult(random(1,5));
+    this.acc = createVector(0, 0);
+    this.r = random(2,5);
+    this.color = random(10,255);
+    this.lifetime = 255;
+  }
 
-    applyForce(force){
-        this.acc.add(force);
-    }
+  finished(){
+      return this.lifetime < 0
+  }
 
-    edges(){
-        // if(this.pos.){
+  applyForce(force) {
+    this.acc.add(force);
+  }
 
-        // }
+  // wall constraints
+  edges() {
+    if (this.pos.y >= height - this.r/2) {
+      // return the ball to height so it wont get stuck!
+      this.pos.y = height - this.r / 2;
+      // reverse ball direction
+      this.vel.y *= -1;
     }
+    if (this.pos.x >= width - this.r / 2) {
+      // return ball to width so it wont get stuck!
+      this.pos.x = width - this.r / 2;
+      // reverse ball direction
+      this.vel.x *= -1;
+    } else if (this.pos.x <= this.r / 2) {
+      // return ball to width so it wont get stuck!
+      this.pos.x = this.r / 2;
+      // reverse ball direction
+      this.vel.x *= -1;
+    }
+  }
+
+  update() {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.set(0, 0);
+    this.lifetime -= 5;
+  }
+
+  show() {
+    stroke(this.lifetime);
+    strokeWeight(2);
+    fill(this.color,255,this.color, this.lifetime);
+    ellipse(this.pos.x, this.pos.y, this.r * 2);
+    line(200,20,this.pos.x,this.pos.y )
+
+
+  }
 }
--- 4_particle_systems/ps1/sketch.js
+++ 4_particle_systems/ps1/sketch.js
@@ -1,23 +1,33 @@
-let mover;
+let particles = [];
 
+function setup() {
+  createCanvas(400, 400);
+ 
+  }
+  
 
-function setup(){
-    createCanvas(400,400);
-    particle = new Particle(200,200);
+function draw() {
+  background(0);
+
+  // if(mouseIsPressed){
+  //     let wind = createVector(0.1,0);
+  //     Particle.applyForce(wind);
+  // }
+  for(let i =0; i < 5; i++){
+    particles.push(new Particle(200, 25, random(100,255)));
 }
+  for (let particle of particles) {
+    let gravity = createVector(0, 0.2);
 
-function draw(){
-    bachground(0)
-
-    if(mouseIsPressed){
-        let wind = createVector(0.1,0);
-        Particle.applyForce(wind);
-    }
-
-    let gravity = createVector(0,0.2);
     particle.applyForce(gravity);
-
     particle.update();
     particle.edges();
     particle.show();
+  }
+
+  for(let i = particles.length-1; i>=0; i--){
+      if (particles[i].finished()){
+          particles.splice(i,1)
+      }
+  }
 }
